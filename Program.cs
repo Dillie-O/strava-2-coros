6c40d3ca111045296a335eeabee591aaa5adf5bc
Sean Patterson <dillieo@gmail.com>
2021-11-03 06:36:38 -0700

    Updated code to reflect library updates. Added heartrate data.

--- Program.cs
+++ Program.cs
@@ -15,17 +15,13 @@ namespace strava_2_coros
             try
             {
                 XDocument myGPX = XDocument.Load("sample.gpx");
-
-                XmlNamespaceManager r = new XmlNamespaceManager(new NameTable());
-                r.AddNamespace("p", "http://www.topografix.com/GPX/1/1");
-
-                StravaGPXReaderLib.GPXReader gPXReader = new StravaGPXReaderLib.GPXReader(myGPX, r);
+                StravaGPXReaderLib.StravaGPXReader gPXReader = new StravaGPXReaderLib.StravaGPXReader(myGPX);
 
                 Console.WriteLine("Name: " + gPXReader.GetGPXName());
                 Console.WriteLine("Activity Type: " + gPXReader.GetActivityType());
-                Console.WriteLine("Avg Elevation: " + gPXReader.GetElevation(StravaGPXReaderLib.GPXReader.ElevationType.Avg));
-                Console.WriteLine("Min Elevation: " + gPXReader.GetElevation(StravaGPXReaderLib.GPXReader.ElevationType.Min));
-                Console.WriteLine("Max Elevation: " + gPXReader.GetElevation(StravaGPXReaderLib.GPXReader.ElevationType.Max));
+                Console.WriteLine("Avg Elevation: " + gPXReader.GetElevation(StravaGPXReaderLib.StravaGPXReader.ElevationType.Avg));
+                Console.WriteLine("Min Elevation: " + gPXReader.GetElevation(StravaGPXReaderLib.StravaGPXReader.ElevationType.Min));
+                Console.WriteLine("Max Elevation: " + gPXReader.GetElevation(StravaGPXReaderLib.StravaGPXReader.ElevationType.Max));
                 Console.WriteLine("Start Date: " + gPXReader.GetStartDt());
                 Console.WriteLine("End Date: " + gPXReader.GetEndDt());
                 Console.WriteLine("Duration: " + gPXReader.GetDuration());
@@ -33,10 +29,10 @@ namespace strava_2_coros
 
                 Console.WriteLine("Printing complete list of latitude - longitude with details");
 
-                List<StravaGPXReaderLib.Models.GPXCoordinates> coordinates = gPXReader.GetGPXCoordinates();
-                for (int i = 0; i < coordinates.Count; i++)
+                List<StravaGPXReaderLib.Models.GPXTrackPoint> trackPoints = gPXReader.GetGPXTrackPoints();
+                for (int i = 0; i < trackPoints.Count; i++)
                 {
-                    Console.WriteLine($"Time: {coordinates[i].Timestamp.ToString()} // lat: {coordinates[i].Latitude} - lon: {coordinates[i].Longitude} \\\\ Elevation: {coordinates[i].Elevation}");
+                    Console.WriteLine($"Time: {trackPoints[i].Timestamp.ToString()} // lat: {trackPoints[i].Coordinates.Latitude} - lon: {trackPoints[i].Coordinates.Longitude} \\\\ Elevation: {trackPoints[i].Elevation} - HeartRate: {trackPoints[i].HeartRate}");
                 }
 
                 // StravaGPXReaderLib.Models.GPXAltimetry altimetry = gPXReader.GetGPXAltimetry();
